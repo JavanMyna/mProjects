6ed86952d20ca95ceae33d826aa5c6aa5e4d2d27
JavanMyna <112639724+JavanMyna@users.noreply.github.com>
2026-09-24 21:53:52 +0800

    Go go go!

---
+++ go-go/gopal/gopal.go
@@ -0,0 +1,8 @@
+package main
+
+//how to do input()?
+func main() {
+
+}
+
+//im tired so im leaving it here okee 24/09/2026
---
+++ go-go/goreng/goreng.go
@@ -0,0 +1,12 @@
+package main //Wait... why? Oh it has to be the same as the function name. In java case, its title of file and the function title must be the same huh.
+
+import "fmt" //so fmt is like a way to #include <iostream> huh, python dont do that because it only interprets, not compile hah
+
+func main() { //func is different too, in python its def
+	fmt.Println("Name: Fred\nCourse: Computer Science\nLanguage: Go") //Its been quite a long time since I used Go, like this looked unfamiliar to me because I mainly vibe coded back then due to time constraint
+}//so fmt. is like std:: in c++ huh
+
+// whats foo() oh any function
+// I made a wonderful discovery why people use void to make function in c++
+// Turns out IT IS ONE OF THE WAY to make functions with voids, I defaulted to put int, std::string or double whenever making function hahaha.
+
